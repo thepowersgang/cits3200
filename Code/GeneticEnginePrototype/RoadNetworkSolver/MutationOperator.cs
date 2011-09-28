183cdf99fec6ff491c9db60149bde8748f940cbb
John Hodge <tpg@mutabah.net>
2011-09-28 12:28:46 +0800

    Working on the mutation operator

--- Code/GeneticEnginePrototype/RoadNetworkSolver/MutationOperator.cs
+++ Code/GeneticEnginePrototype/RoadNetworkSolver/MutationOperator.cs
@@ -1,10 +1,32 @@
 using System;
+using RoadNetworkSolver;
+
 namespace RoadNetworkSolver
 {
-	public class MutationOperator
+	public class MutationOperator: IGeneticOperator<MutationOperator>
 	{
-		public MutationOperator ()
+		public MutationOperator()
 		{
+		}
+		
+		public void Operate(IGeneration Population, ArrayList List)
+		{
+			for(RoadNetwork ent in Population)
+			{
+				Mutate(ent);
+				List.add(ent);
+			}
+		}
+		
+		private void Mutate(RoadNetwork ent)
+		{
+			for( int ii = 1; ii < ent.VertexCount; ii ++ )
+			{
+				Vertex v = net.GetVertex(ii);
+				v.Coordinates.x += Random(-5, 5);
+				// TODO: Clip values
+				v.Coordinates.y += Random(-5, 5);
+			}
 		}
 	}
 }
