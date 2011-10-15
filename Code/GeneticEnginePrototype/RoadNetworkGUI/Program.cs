4af2bed38e038f530d9a1e896a08821c666ae084
Brian Marshall <bmarshall@iinet.net.au>
2011-10-15 12:55:21 +0800

    added example XML
    Signed-off-by: Brian Marshall <bmarshall@iinet.net.au>

--- Code/GeneticEnginePrototype/RoadNetworkGUI/Program.cs
+++ Code/GeneticEnginePrototype/RoadNetworkGUI/Program.cs
@@ -18,6 +18,7 @@ namespace RoadNetworkGUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new RoadNetworkFinder());
+            //Application.Run(new Road_Network_Visualiser(false, null));
         }
     }
 }
