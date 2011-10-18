09b3ac75f232fb7bc42d21535efd3fadbd981ebf
Brian Marshall <bmarshall@iinet.net.au>
2011-10-18 14:35:44 +0800

    Moved MutationOperator out of RoadNetworkSolver. Sorry John, it was crashing the engine.

--- Code/GeneticEnginePrototype/GeneticEngineTesting/TestClass.cs
+++ Code/GeneticEnginePrototype/GeneticEngineTesting/TestClass.cs
@@ -232,7 +232,7 @@ namespace GeneticEngineTesting
         [TestMethod]
         public void TestD1()
         {
-            MutationOperator RNCO = new MutationOperator(null);
+            Mutator RNCO = new Mutator(null);
             AATreeGeneration generation = new AATreeGeneration();
             ArrayList individuals = new ArrayList();
             //Populate with identical entries.
